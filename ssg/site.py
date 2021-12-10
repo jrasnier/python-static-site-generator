059b297aa7b4f32137ac1bff8cf51d4df5eac038
Joshua Rasnier <joshua.rasnier@global.ntt>
2021-12-10 11:46:53 +1100

    New file

---
+++ ssg/site.py
@@ -0,0 +1,8 @@
+import path from pathlib
+
+class Site(self, source, dest):
+    source = path(source)
+    dest = path(dest)
+
+   def create_dir(self, path):
+       directory = self.dest() + "/" + relative_to()
