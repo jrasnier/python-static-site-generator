9a5a6849116c67d77880585e03802f3cb72183f0
Joshua Rasnier <jrasnier@gmail.com>
2021-12-12 01:46:22 +1100

    Edit parser

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -23,7 +23,7 @@ class Parser():
     def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
 
-class ResourceParser(extensions):
+class ResourceParser(Parser):
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
     def parse(self, path: Path, source, dest):
