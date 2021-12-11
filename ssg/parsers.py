63325df1d9ed3af8393374066bcab6c8d64ff71e
Joshua Rasnier <jrasnier@gmail.com>
2021-12-12 01:23:37 +1100

    Edit parser

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,5 +1,6 @@
 from typing import List
 from pathlib import Path
+import shutil
 
 class Parser():
     extensions: List[str] = []
@@ -17,5 +18,7 @@ class Parser():
     def write(self, path, dest, content, ext = ".html"):
         full_path = dest / path.with_suffix(ext).name
         with open(full_path, "w") as file:
-            file.write(content)
+            file.write(content
 
+    def copy(self, path, source, dest):
+        self.copy2(path, dest / path.relative_to(source))
