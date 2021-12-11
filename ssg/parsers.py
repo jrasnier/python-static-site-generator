b5e000373cc1f617bce1bd52034e1c173d3a7f5a
Joshua Rasnier <jrasnier@gmail.com>
2021-12-12 01:40:00 +1100

    Edit parser

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -22,3 +22,9 @@ class Parser():
 
     def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
+
+class ResourceParser(extensions):
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
