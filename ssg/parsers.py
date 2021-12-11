42702ce478b98722521fd224b6747665f90f3816
Joshua Rasnier <jrasnier@gmail.com>
2021-12-12 00:55:01 +1100

    Edit parser

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -3,3 +3,11 @@ from pathlib import Path
 
 class Parser():
     extensions: List[str] = []
+
+    def valid_extension(self, extension):
+        return extension in self.extensions
+
+    def parse(path, source, dest):
+        path: Path[]
+        source: Path[]
+        dest: Path[]
