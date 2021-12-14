cd55199a2bbcd206c44f5220e9ec5c075f7b855f
Joshua Rasnier <jrasnier@gmail.com>
2021-12-15 00:37:47 +1100

    Add Content

--- ssg/content.py
+++ ssg/content.py
@@ -7,10 +7,14 @@ class Content(Mapping):
     __delimiter = r"^(?:-|\+){3}\s*$"
     __regex  = re.compile(__delimiter, re.MULTILINE)
 
-    def load(cls, string):
+    @classmethod
+    def load(self, cls, string):
         __regex = split(string, 2)
         _ = __regex
         fm = __regex
         content = __regex
+
+        self.load(fm)
+        return cls(metadata, content)
 
 
