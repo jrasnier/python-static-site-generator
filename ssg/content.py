e6161c320a78a042a5f8ca0d0c0032555c0abd62
Joshua Rasnier <jrasnier@gmail.com>
2021-12-14 22:45:48 +1100

    Add Content

---
+++ ssg/content.py
@@ -0,0 +1,10 @@
+import re
+from yaml import load
+from yaml import FullLoader
+from collections.abc import Mapping
+
+class Content(Mapping):
+    __delimeter = "^(?:-|\+){3}\s*$"
+    __regex  = re.compile(__delimiter, re.MULTILINE)
+
+
