7008da172ac3fb65b4e7d43f2223769f81250c1b
Joshua Rasnier <jrasnier@gmail.com>
2021-12-16 00:46:15 +1100

    Add Content

--- ssg/content.py
+++ ssg/content.py
@@ -27,7 +27,7 @@ class Content(Mapping):
         return self.data["type"] if "type" in self.data else None
         
     @type.setter
-    def type(self):
+    def type(self, type):
         self.data["type"] = type
 
     def __getitem__(self, key):
