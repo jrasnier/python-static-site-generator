2724840a4848c0d6cc4a70b5968ff3194038dde4
Joshua Rasnier <jrasnier@gmail.com>
2021-12-19 01:19:31 +1100

    Edit Parser

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -41,3 +41,12 @@ class MarkdownParser(Parser):
         html = markdown(content.body())
         self.write(path, dest, html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+
+class ReStructuredTextParser(Parser):
+    extensions = [".rst"]
+
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
+        html = publish_parts(content.body(writer_name: "html5"))
+        self.write(html["html_body"], path, dest)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
